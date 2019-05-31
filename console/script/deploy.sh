3db2be42ef4b6822e3aa9ad21967cec85c790161
spirit <catchspirit37@gmail.com>
2019-05-31 15:31:13 +0800

    z - Fri May 31 2019 15:31:13 GMT+0800 (CST)

--- console/script/deploy.sh
+++ console/script/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "connecting to server: $1"
-ssh root@"$1" "curl https://raw.githubusercontent.com/spirit1453/script/master/task/deployConsole.sh|bash"
+ssh root@"$1" "java --version || curl https://raw.githubusercontent.com/spirit1453/script/master/task/deployConsole.sh|bash"
+open "https://$1"
