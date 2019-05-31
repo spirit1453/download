bcbb2e4367486c6ebf9464cc33ab3456aa3abbb4
spirit <catchspirit37@gmail.com>
2019-05-31 10:29:20 +0800

    z - Fri May 31 2019 10:29:20 GMT+0800 (CST)

--- console/script/upload.sh
+++ console/script/upload.sh
@@ -6,11 +6,16 @@ SRCDIR=$(dirname "$PWD")"/generated"
 DESDIR=/opt/testing/LK-S/static/other
 #输入上传文件名称
 FILE="console.zip"
+cached="console_cached.zip"
+old="console_old.zip"
 sftp root@192.144.200.234<< EOF
 cd ${DESDIR}/
 lcd ${SRCDIR}
-put ${FILE}
-by
+put ${FILE} ${cached}
+rm ${old}
+rename ${FILE} ${old}
+rename ${cached} ${FILE}
+exit
 EOF
 
 echo "upload console.zip to server successfully"
