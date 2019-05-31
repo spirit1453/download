b9da4179d060b3d1177c339a202d48ad6f7cd6ba
spirit <catchspirit37@gmail.com>
2019-05-31 09:28:42 +0800

    z - Fri May 31 2019 09:28:42 GMT+0800 (CST)

--- console/script/sftp.sh
+++
@@ -1,1 +0,0 @@
- sftp -b script/upload.sh root@192.144.200.234
--- console/script/upload.sh
+++ console/script/upload.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-source config
-
 #本地目录
 SRCDIR=$(dirname "$PWD")"/generated"
 #FTP目录
 DESDIR=/opt/testing/LK-S/static/other
 #输入上传文件名称
-read -p "Input upload file name:" filename
-FILE=$filename
-sftp -P ${port} ${username}@${ip} << EOF
+FILE="console.zip"
+sftp root@192.144.200.234<< EOF
 cd ${DESDIR}/
 lcd ${SRCDIR}
 put ${FILE}
 by
 EOF
-echo "commit to ftp successfully"
+
+echo "upload console.zip to server successfully"
